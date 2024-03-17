2b6f8ae00ac37280482888eb2a3924eb3fa05514
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2024-03-17 08:53:19 -0300

    done

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ const express = require('express');
 // Handles the handlebars
 // https://www.npmjs.com/package/hbs
 const hbs = require('hbs');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoStore = require("connect-mongo")(session);
 
 const app = express();
 
@@ -19,12 +22,26 @@ const app = express();
 require('./config')(app);
 
 // default value for title local
-const projectName = 'lab-express-basic-auth';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
+// const projectName = 'lab-express-basic-auth';
+// const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
-app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
+// app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
+
+app.use(session({
+  secret: 'basic-auth-secret',
+  cookie: { maxAge: 60 * 1000 },
+  resave: true,
+  saveUnitialized: false,
+  store: new MongoStore({
+    mongooseConnection: mongoose.connection,
+    ttl: 24 * 60 * 60
+  })
+}));
 
 // 👇 Start handling routes here
+const auth = require('./routes/auth');
+app.use('/', auth);
+
 const index = require('./routes/index');
 app.use('/', index);
 
--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,20 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+router.use((req, res, next) => {
+  if (req.session.currentUser) {
+    next()
+  } else {
+    res.redirect('/login');
+  }
+});
+
+router.get('/main', (req, res, next) => {
+  res.render('main');
+})
+
+router.get('/private', (req, res, next) => {
+  res.render('private')
+})
+
 module.exports = router;
